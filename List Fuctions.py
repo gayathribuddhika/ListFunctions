1aad0a93347aa4d9a4808a73cbd78ceb957462f3
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-29 23:05:24 +0530

    List Functions
    List Functions in Python

---
+++ List Fuctions.py
@@ -0,0 +1,20 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numbers = [12,45,63,89,197,38,268]
+>>> len(numbers)
+7
+>>> min(numbers)
+12
+>>> max(numbers)
+268
+>>> list("university")
+['u', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+>>> numbers
+[12, 45, 63, 89, 197, 38, 268]
+>>> numbers[2]=24
+>>> numbers
+[12, 45, 24, 89, 197, 38, 268]
+>>> del numbers[4]
+>>> numbers
+[12, 45, 24, 89, 38, 268]
+>>> 
